9465c4b0ec77a10599321d60ad8395d03bb6840a
jjjjinwoo <somes228@gmail.com>
2025-03-26 09:49:05 +0900

    Menu Delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MyComponent from './components/MyComponent';
-import Menu from './components/Menu';
 
 function App(){
   return (
     <div className='App'>
       <MyComponent />
-      <Menu />
     </div>
   )
 }
